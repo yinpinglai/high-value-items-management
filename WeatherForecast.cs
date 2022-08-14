f5fb42eb51bc815f31b6b12ade4fd4457e9c52b1
Yin Ping Lai <ypinglai29@gmail.com>
2022-08-13 21:33:12 -0600

    First commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace nude_solutions_developer_assignment;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
